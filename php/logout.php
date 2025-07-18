6e5a21d2d2c45dc7427081b19a4c1de07e2bd512
Shen <119427365+Shen220@users.noreply.github.com>
2025-07-18 17:59:30 +0800

    Delete php directory

--- php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
-header("Location: authentication.php"); // Redirect to login page
-exit();
